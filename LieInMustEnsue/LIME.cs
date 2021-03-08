9cdb6e867fa52cfada3a072a472130126141f355
FruitGooseKSP <jrf091981@gmail.com>
2021-03-08 17:03:23 +0000

    Initial

---
+++ LieInMustEnsue/LIME.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace LieInMustEnsue
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class LIME : MonoBehaviour
+    {
+        [KSPField]
+        public double snoozeTime;
+
+        public void Start()
+        {
+            snoozeTime = 0.467;                                         // an hour later than dawn
+            KSP.UI.UIWarpToNextMorning.timeOfDawn = snoozeTime;
+        }
+    }
+}
